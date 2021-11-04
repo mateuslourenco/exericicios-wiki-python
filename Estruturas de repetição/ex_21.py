00b8d3da18db62bd1aa1fd6fb9f5aaecf6b497bb
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:27:19 -0300

    resolvido exercício 21 estrutura de repetição

--- Estruturas de repetição/ex_21.py
+++ Estruturas de repetição/ex_21.py
@@ -1,3 +1,14 @@
 """Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que
  é divisível somente por ele mesmo e por 1.
 """
+
+numero = int(input('Infome um número inteiro: '))
+divisores = 0
+for divisor in range(numero, 0, -1):
+    if numero % divisor == 0:
+        divisores += 1
+
+if divisores == 2:
+    print('primo')
+else:
+    print('não é primo')
