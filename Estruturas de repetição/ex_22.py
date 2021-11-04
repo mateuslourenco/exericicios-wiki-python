c2bcfd65caba03ab4880deacf131b134d3df5645
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:32:13 -0300

    resolvido exercício 22 estrutura de repetição

--- Estruturas de repetição/ex_22.py
+++ Estruturas de repetição/ex_22.py
@@ -1,4 +1,17 @@
 """Altere o programa de cálculo dos números primos, informando, caso o número não seja primo, por quais número ele é
 divisível.
 """
+numero = int(input('Infome um número inteiro: '))
+divisores = 0
+numeros_divisores = []
+for divisor in range(numero, 0, -1):
+    if numero % divisor == 0:
+        divisores += 1
+        numeros_divisores.append(divisor)
 
+
+if divisores == 2:
+    print('primo')
+else:
+    print('não é primo')
+    print(numeros_divisores)
