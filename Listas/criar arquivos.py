f12bc894a70619ee5c7ad1600896eefd97806764
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:37:26 -0300

    resolvido exercício 1 listas

--- Listas/criar arquivos.py
+++ Listas/criar arquivos.py
@@ -1,2 +1,2 @@
-for i in range(1, 52):
+for i in range(1, 25):
     arquivo = open(f'ex_{i:02}.py', 'a')
---
+++ Listas/ex_01.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+"""
+numeros = []
+for _ in range(5):
+    numeros.append(int(input('Informe um número inteiro: ')))
+
+print(numeros)
