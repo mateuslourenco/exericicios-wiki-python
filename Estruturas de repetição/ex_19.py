095ad40722c7753dc2556cf99703548e7feb90f3
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:05:41 -0300

    resolvido exercício 19 estrutura de repetição

--- Estruturas de repetição/ex_19.py
+++ Estruturas de repetição/ex_19.py
@@ -1,2 +1,24 @@
 """Altere o programa anterior para que ele aceite apenas números entre 0 e 1000.
 """
+numero = int(input('Informe um número ou -1 para sair: '))
+menor = numero
+maior = numero
+soma = numero
+
+while True:
+    numero = int(input('Informe um número ou -1 para sair: '))
+    if numero == -1:
+        break
+    elif numero not in range(1001):
+        print('O número de estar entre 0 e 1000')
+    else:
+        if numero > maior:
+            maior = numero
+        elif numero < menor:
+            menor = numero
+        soma += numero
+
+print(f'Menor: {menor}')
+print(f'Maior: {maior}')
+print(f'Soma: {soma}')
+
