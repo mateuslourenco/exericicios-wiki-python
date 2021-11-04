a95358d7b625ef747460daef38d7baf3dbaf08c1
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:09:42 -0300

    resolvido exercício 20 estrutura de repetição

--- Estruturas de repetição/ex_20.py
+++ Estruturas de repetição/ex_20.py
@@ -1,3 +1,16 @@
 """Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial várias vezes e limitando o
 fatorial a números inteiros positivos e menores que 16.
 """
+
+while True:
+    numero = int(input('Informe o número ou -1 para sair: '))
+    if numero == -1:
+        break
+    elif numero > 16:
+        print('O número deve ser menor que 16')
+    else:
+        fatorial = numero
+        while numero > 1:
+            fatorial *= numero - 1
+            numero -= 1
+        print(fatorial)
