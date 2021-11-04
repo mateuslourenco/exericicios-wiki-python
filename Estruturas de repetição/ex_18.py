60a12ff97f73b04a2fe80dfef8462e0f7caea5ae
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-04 11:02:04 -0300

    resolvido exercício 18 estrutura de repetição

--- Estruturas de repetição/ex_18.py
+++ Estruturas de repetição/ex_18.py
@@ -1,2 +1,23 @@
 """Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
 """
+
+numero = int(input('Informe um número ou -1 para sair: '))
+menor = numero
+maior = numero
+soma = numero
+
+while True:
+    numero = int(input('Informe um número ou -1 para sair: '))
+    if numero == -1:
+        break
+    elif numero > maior:
+        maior = numero
+    elif numero < menor:
+        menor = numero
+
+    soma += numero
+
+print(f'Menor: {menor}')
+print(f'Maior: {maior}')
+print(f'Soma: {soma}')
+
