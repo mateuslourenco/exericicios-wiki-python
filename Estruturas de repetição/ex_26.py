237c8ad270e75d54fb7618dab6ac0be86bf62842
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-28 18:13:27 -0300

    resolvido exercício #26 repeticao

--- Estruturas de repetição/ex_26.py
+++ Estruturas de repetição/ex_26.py
@@ -1,3 +1,17 @@
 """Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor
 votar e ao final mostrar o número de votos de cada candidato.
 """
+
+numero_de_eleitores = int(input('Informe o número de eleitores: '))
+votos = {'1': 0, '2': 0, '3': 0}
+
+for eleitor in range(numero_de_eleitores):
+    print(f'{ "*" * 10} Candidatos {"*" * 10}')
+    print('Digite 1 para votar no candidato 1')
+    print('Digite 2 para votar no candidato 2')
+    print('Digite 3 para votar no candidato 3')
+    voto = input('Votar: ')
+    votos[voto] += 1
+
+for candidato, total_votos in votos.items():
+    print(f'Total de {total_votos} para candidato {candidato}')
