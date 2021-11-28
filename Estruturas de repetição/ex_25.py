e5fef731d4956607805a9bc8a9d83483b1acfd63
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-28 18:04:33 -0300

    resolvido exercício #25 repeticao

--- Estruturas de repetição/ex_25.py
+++ Estruturas de repetição/ex_25.py
@@ -2,3 +2,21 @@
 turma varia entre 0 e 25,26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa, conforme a média
 calculada.
 """
+idades = []
+while True:
+    opcao = int(input('Digite a idade ou digite -1 para sair: '))
+    if opcao != -1:
+        idades.append(opcao)
+    else:
+        break
+
+media = sum(idades) / len(idades)
+
+if media > 60:
+    print('idosa')
+elif media >= 26:
+    print('adulta')
+elif media >= 0:
+    print('jovem')
+else:
+    print('idades invalidas')
