dbdb3e85d0ce3788310dec5c6e353302ff6c9afd
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-28 18:35:18 -0300

    resolvido exercício #28 repeticao

--- Estruturas de repetição/ex_28.py
+++ Estruturas de repetição/ex_28.py
@@ -2,3 +2,12 @@
 em cada um deles. O usuário deverá informar a quantidade de CDs e o valor para em cada um.
 """
 
+qtd_cds = int(input('Quantidade de CDs: '))
+valor_total = 0
+for cd in range(qtd_cds):
+    valor = float(input(f'Informe o valor do cd {cd + 1}: '))
+    valor_total += valor
+
+media = valor_total / qtd_cds
+print(f'Total investido: {valor_total}')
+print(f'Valor médio: {media}')
