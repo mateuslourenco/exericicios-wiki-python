89aabe28664fb8440190dfe17803bc6763944444
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-28 17:56:12 -0300

    resolvido exercício #23 repeticao

--- Estruturas de repetição/ex_23.py
+++ Estruturas de repetição/ex_23.py
@@ -1,3 +1,16 @@
 """Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário. O programa
 deverá mostrar também o número de divisões que ele executou para encontrar os números primos. Serão avaliados o
 funcionamento, o estilo e o número de testes (divisões) executados."""
+
+numero_n = int(input('Informe um número inteiro: '))
+primos = []
+
+for numero in range(1, numero_n + 1):
+    contador = 0
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            contador += 1
+    if contador == 2:
+        primos.append(numero)
+
+print(primos)
