028505896226783172cb1b65e768de52d862d844
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-12-11 01:17:55 -0300

    segunda tentativa

--- entrevistas/desafio_bud.py
+++ entrevistas/desafio_bud.py
@@ -11,28 +11,31 @@ def encontrar_maior_numero_digitos_consecutivos(numero):
     >>> encontrar_maior_numero_digitos_consecutivos(123789)
     789
 
-    :param numero: 
-    :return: 
+    :param numero:
     """
     prox_numero_dict = {
         '0': '1', '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '0'
     }
     numero_str = str(numero)
-    maior_numero = ''
-    prox_digito = numero_str[1]
-    total_numeros_localizados = []
-    for digito in numero_str:
-        if prox_digito == prox_numero_dict[digito]:
-            maior_numero += digito
-            prox_digito = str(int(prox_digito) + 1)
-        else:
-            total_numeros_localizados.append(int(maior_numero))
-            prox_digito = str(int(digito) + 1)
-            maior_numero = digito
+    maiores_numeros = []
+    numero_localizado = ''
+    for i, num in enumerate(numero_str):
+        try:
+            if numero_str[i + 1] == prox_numero_dict[num]:
+                numero_localizado += num
+            else:
+                numero_localizado += num
+                maiores_numeros.append(int(numero_localizado))
+                numero_localizado = ''
+        except IndexError:
+            if numero_str[i] == prox_numero_dict[num]:
+                numero_localizado += num
+                maiores_numeros.append(int(numero_localizado))
+                maiores_numeros.append(int(numero_localizado))
 
-    maior_numero_localizado = max(total_numeros_localizados)
+    maior_numero_localizado = max(maiores_numeros)
     print(maior_numero_localizado)
 
 
 if __name__ == '__main__':
-    encontrar_maior_numero_digitos_consecutivos(1237890)
+    encontrar_maior_numero_digitos_consecutivos(123789)
