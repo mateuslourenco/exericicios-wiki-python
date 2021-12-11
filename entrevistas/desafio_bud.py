4a5331cf80d7a139291e4714758cd031c7894187
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-12-11 02:59:01 -0300

    desafio concluido

--- entrevistas/desafio_bud.py
+++ entrevistas/desafio_bud.py
@@ -17,23 +17,23 @@ def encontrar_maior_numero_digitos_consecutivos(numero):
 
     :param numero:
     """
-    prox_numero_dict = {
+    prox_digito_dict = {
         '0': '1', '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '0'
     }
     numero_str = str(numero)
     maiores_numeros = []
     numero_localizado = ''
-    for i, num in enumerate(numero_str):
+    for i, digito in enumerate(numero_str):
         try:
-            if numero_str[i + 1] == prox_numero_dict[num]:
-                numero_localizado += num
+            if numero_str[i + 1] == prox_digito_dict[digito]:
+                numero_localizado += digito
             else:
-                numero_localizado += num
+                numero_localizado += digito
                 maiores_numeros.append(int(numero_localizado))
                 numero_localizado = ''
         except IndexError:
-            if prox_numero_dict[num] == prox_numero_dict[numero_str[i]]:
-                numero_localizado += num
+            if prox_digito_dict[digito] == prox_digito_dict[numero_str[i]]:
+                numero_localizado += digito
                 maiores_numeros.append(int(numero_localizado))
 
     maior_numero_localizado = max(maiores_numeros)
@@ -41,4 +41,4 @@ def encontrar_maior_numero_digitos_consecutivos(numero):
 
 
 if __name__ == '__main__':
-    encontrar_maior_numero_digitos_consecutivos(123789)
+    encontrar_maior_numero_digitos_consecutivos(123)
